2babd3c97ac0b458f7cc3138214271362f4c0761
Max Sotnikov <felaw@yandex.ru>
2021-12-03 15:50:59 +0300

    fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,31 @@
 const edit = document.querySelector('.profile__edit-button');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.modal__close-btn');
-const modalInner = document.querySelector('.modal__inner');
-
-let nameInput = document.querySelector('.js-name');
-let jobInput = document.querySelector('.js-job');
+const popup = document.querySelector('.modal__profile');
+const save = document.querySelector('.modal__submit');
+let nameInput = document.querySelector('.modal__input_type_name');
+let jobInput = document.querySelector('.modal__input_type_job');
 let newName = document.querySelector('.profile__title');
 let newJob = document.querySelector('.profile__subtitle');
 
-function openModal() {
+function openModal() { //открытие попап
     modal.classList.add('modal_active');
-    document.body.classList.add('hidden');
-    modalInner.classList.add('fade-in');
+    nameInput.value = newName.textContent;
+    jobInput.value = newJob.textContent;
 }
 
-function closeModal() {
+function closeModal() { //закрытие попап
     modal.classList.remove('modal_active');
-    document.body.classList.remove('hidden');
 }
 
-function overlayClick(e) {
-    if (!modalInner.contains(e.target)) {
-        closeModal();
-    }
-}
-
-function formSubmitHandler(evt) {
+function formSubmitHandler(evt) { //заполнение профиля
     evt.preventDefault();
     newName.textContent = nameInput.value;
     newJob.textContent = jobInput.value;
+    save.contains(evt.target);
     closeModal();
 }
 
 edit.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
-modal.addEventListener('click', overlayClick);
-modalInner.addEventListener('submit', formSubmitHandler);
+popup.addEventListener('submit', formSubmitHandler);
